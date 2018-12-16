fdff0b38bb6d01a0e32ca98f58550ff1cf2a9648
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2018-12-16 22:26:04 +0000

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 
-DEFINE ('DB_SERVER','collycomp.uk');
-DEFINE ('DB_USER','15ritchingd03');
-DEFINE ('DB_PASSWORD','SY156675');
-DEFINE ('DB_NAME', '15ritchingd03_3');
+DEFINE ('DB_SERVER','den1.mysql2.gear.host');
+DEFINE ('DB_USER','schooltimetable');
+DEFINE ('DB_PASSWORD','Fs6b0?RH?x37');
+DEFINE ('DB_NAME', 'schooltimetable');
 
 $dbc=@mysqli_connect (DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME) OR die ('Connection failed: '.mysqli_connect_error());
 
